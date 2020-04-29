6d9273f6895410b49c7e524a5717d6c2b6cd4c19
beardykraken <vcasmirri@gmail.com>
2020-04-29 14:22:51 -0400

    completed configuring htmlroutes.js

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,25 +1,16 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
+// set our dependency
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var path = require("path");
 
 module.exports = function(app) {
 
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
+  // API routes for visiting pages
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
-  // If no matching route is found default to home
+  // default to home if no matching route is found
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
--- server.js
+++ server.js
@@ -9,24 +9,16 @@ var app = express();
 // set the port we'll use for our listener
 var PORT = process.env.PORT || 8080;
 
-// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
+// express.json and express.urlEncoded helps our server interpret data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+//  route files for our server
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// ==============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// ==============================================================================
+// listener
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
