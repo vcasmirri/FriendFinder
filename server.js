b7285db7e6e8c0b1ea4cfe0ce3186aa80c5c42f4
beardykraken <vcasmirri@gmail.com>
2020-04-29 14:14:51 -0400

    Created initial files and directories.

---
+++ server.js
@@ -0,0 +1,33 @@
+// require express
+
+var express = require("express");
+
+// create an express server
+
+var app = express();
+
+// set the port we'll use for our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
